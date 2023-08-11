7d07b24f611e5ec9151cedd82c64147cecc10c00
Brian <brian.stublen@gmail.com>
2023-08-11 11:31:12 -0400

    Testing Streamlit

--- doc_chat_common.py
+++ doc_chat_common.py
@@ -6,6 +6,28 @@ import openai
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket('bfs-chat')
 
+def get_secret(secret_name, region_name):
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = json.loads(get_secret_value_response['SecretString'])
+
+    return secret
+
 def set_api_key():
     os.environ["OPENAI_API_KEY"] = get_secret('OPENAI_API_KEY', 'us-east-1')['OPENAI_API_KEY']
 
@@ -39,24 +61,4 @@ def get_collection_files(collection):
     files.sort()
     return files
 
-def get_secret(secret_name, region_name):
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = json.loads(get_secret_value_response['SecretString'])
-
-    return secret
