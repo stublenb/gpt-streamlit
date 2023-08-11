767449ab9f1472bf8ef04074124f6302946f315a
Brian <brian.stublen@gmail.com>
2023-08-11 08:39:42 -0400

    Testing Streamlit

--- 1_About.py
+++ 1_About.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.title("Hello World")
-st.write("Hello World")
+st.title("Chat-GPT Document Intelligence POC")
+st.write("This is a proof of concept to leverage Chat-GPT to analyze documents")
+st.write("File Loader - Create a document collection and upload documents to derive insights from")
+st.write("Collection Manager - Delete documents from collections")
+st.write("Document Collection Chat - Openly ask questions pertaining to the selected collection of documents")
