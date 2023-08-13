c5777f9d5579dee31c0bdaaf3a863fa98fa772e3
Brian <brian.stublen@gmail.com>
2023-08-13 10:24:00 -0400

    Testing Streamlit

--- pages/4_Document_Collection_Chat.py
+++ pages/4_Document_Collection_Chat.py
@@ -26,7 +26,7 @@ model = st.sidebar.selectbox(
     list(chain_options.keys())
 )
 
-st.sidebar.write(template)
+template = st.sidebar.text_input(value = template)
 # def reset_conversation():
 #   st.session_state.conversation = None
 #   st.session_state.chat_history = None
