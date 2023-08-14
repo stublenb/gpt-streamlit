c3f9f0ee05d23acee1c7cbf1a4d3c164b38a95c0
Brian <brian.stublen@gmail.com>
2023-08-14 10:51:49 -0400

    Testing Streamlit

--- pages/4_Document_Collection_Chat.py
+++ pages/4_Document_Collection_Chat.py
@@ -9,7 +9,7 @@ import doc_chat_common
 import os
 
 
-template = """You are an AI assistant for answering questions about the software license agreements.
+default_template = """You are an AI assistant for answering questions about the software license agreements.
 You are given the following extracted parts of a long document and a question. Provide a conversational answer.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
 If the question is not about the contracts or license agreements, politely inform them that you are tuned to only answer questions about the contracts or license agreements that are loaded into the database.
